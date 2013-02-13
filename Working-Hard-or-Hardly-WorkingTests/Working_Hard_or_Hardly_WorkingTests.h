c03bd14252fcc53e1ea401d52bdc0543a244829e
Chloé Laisné <chloe.laisne@hyperisland.se>
2013-02-13 13:46:31 +0100

    Initial Commit

---
+++ Working-Hard-or-Hardly-WorkingTests/Working_Hard_or_Hardly_WorkingTests.h
@@ -0,0 +1,13 @@
+//
+//  Working_Hard_or_Hardly_WorkingTests.h
+//  Working-Hard-or-Hardly-WorkingTests
+//
+//  Created by Chloe Laisne on 2/13/13.
+//  Copyright (c) 2013 Chloé Laisné. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Working_Hard_or_Hardly_WorkingTests : SenTestCase
+
+@end
---
+++ Working-Hard-or-Hardly-WorkingTests/Working_Hard_or_Hardly_WorkingTests.m
@@ -0,0 +1,32 @@
+//
+//  Working_Hard_or_Hardly_WorkingTests.m
+//  Working-Hard-or-Hardly-WorkingTests
+//
+//  Created by Chloe Laisne on 2/13/13.
+//  Copyright (c) 2013 Chloé Laisné. All rights reserved.
+//
+
+#import "Working_Hard_or_Hardly_WorkingTests.h"
+
+@implementation Working_Hard_or_Hardly_WorkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Working-Hard-or-Hardly-WorkingTests");
+}
+
+@end
